8677ced0bc7c01d6fd23b3dc5090e7035049b862
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2023-12-29 18:56:55 +0100

    Adds fixes

--- src/components/StepOne.tsx
+++ src/components/StepOne.tsx
@@ -4,7 +4,7 @@ export default function StepOne() {
 	return (
 		<>
 			<div className="bg-cyan-100 min-h-screen">
-				<div className="container grid grid-cols-2 px-8 py-12 mx-auto">
+				<div className="container grid grid-cols-2 px-12 py-12 mx-auto w-1/2 gap-8">
 					<div className="bg-[url('../../multi-step-form-main/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat min-h-[85vh] bg-white]">
 						<div className="flex flex-cols ">
 							<button className="rounded-full bg-white p-3 m-8">1</button>
@@ -35,8 +35,8 @@ export default function StepOne() {
 							</div>
 						</div>
 					</div>
-					<div className="py-3 flex flex-col">
-						<span className="font-bold text-4xl text-blue-600">
+					<div className="py-3 flex flex-col w-1/2">
+						<span className="font-bold text-4xl text-blue-600 ">
 							Personal info
 						</span>
 						<span className="text-gray mt-5 text-slate-400 text-xl ">
