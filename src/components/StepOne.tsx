5cacc18e7845a3c404eecd4864720d0ca31fb45c
AchrafHann <achraf.hann@gmail.com>
2024-01-03 02:39:23 -0500

    fix positionning issue

--- src/components/StepOne.tsx
+++ src/components/StepOne.tsx
@@ -1,50 +1,25 @@
-/** @format */
+import OverView from "./OverView";
 
 export default function StepOne() {
-	return (
-		<>
-			<div className="bg-cyan-100 min-h-screen">
-				<div className="container grid grid-cols-2 px-12 py-12 mx-auto w-1/2 gap-8">
-					<div className="bg-[url('../../multi-step-form-main/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat min-h-[85vh] bg-white]">
-						<div className="flex flex-cols ">
-							<button className="rounded-full bg-white p-3 m-8">1</button>
-							<div className="mt-8">
-								<div className="text-white text-bold">Step 1</div>
-								<div className="text-white text-bold">YOUR INFO</div>
-							</div>
-						</div>
-						<div className="flex flex-rows ">
-							<button className="rounded-full bg-white p-3 m-8">2</button>
-							<div className="mt-8 ">
-								<div className="text-white text-bold">Step 2</div>
-								<div className="text-white text-bold">SELECT PLAN</div>
-							</div>
-						</div>
-						<div className="flex flex-rows ">
-							<button className="rounded-full bg-white p-3 m-8">3</button>
-							<div className="mt-8 ">
-								<div className="text-white text-bold">Step 3</div>
-								<div className="text-white text-bold">ADD-ONS</div>
-							</div>
-						</div>
-						<div className="flex flex-rows ">
-							<button className="rounded-full bg-white p-3 m-8">4</button>
-							<div className="mt-8 ">
-								<div className="text-white text-bold">Step 4</div>
-								<div className="text-white text-bold">SUMMARY</div>
-							</div>
-						</div>
-					</div>
-					<div className="py-3 flex flex-col w-1/2">
-						<span className="font-bold text-4xl text-blue-600 ">
-							Personal info
-						</span>
-						<span className="text-gray mt-5 text-slate-400 text-xl ">
-							Please provide your name, email address, and phone number
-						</span>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const imgUrl = `bg-[url('../../multi-step-form-main/assets/images/bg-sidebar-desktop.svg')]`;
+  return (
+    <main className="bg-cyan-50 h-screen flex items-center justify-center">
+      <section className="container flex flex-row w-3/4 h-4/5 bg-white rounded-md shadow-md relative">
+        <div
+          className={`absolute top-3 bottom-3 left-3 w-1/4 ${imgUrl} rounded-md flex flex-col items-start pl-4 pt-14`}
+        >
+          <OverView num={1} text="Your Info" />
+          <OverView num={2} text="Select Plan" />
+          <OverView num={3} text="add-ons" />
+          <OverView num={4} text="Summary" />
+        </div>
+        <div className="w-3/5 absolute bottom-3 right-10 top-10 flex flex-col">
+          <h1 className="font-bold text-4xl text-blue-600">Personal info</h1>
+          <p className="text-gray mt-5 text-slate-400 text-lg">
+            Please provide your name, email address, and phone number
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
